db2be03ddf11aa44aaa95e516a20b1c1b3c668c6
gagarin2017 <yury_gagarin2017@mail.ru>
2020-04-29 20:34:47 +0100

    Spring Initilizer

---
+++ Balance_Manager/src/test/java/com/greenland/balancemanager/BalanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenland.balancemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
